44915996405d08012f6eb20c957d996adae8b803
AGBENYA Koffi Moïse <kevagb03@gmail.com>
2020-10-18 20:37:21 +0200

    add consignment client cli

---
+++ shippy-cli-consignment/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+
+	pb "github.com/lordkevinmo/consignment/shippy-service-consignment/proto/consignment"
+	"google.golang.org/grpc"
+)
+
+const (
+	address         = "localhost:50051"
+	defaultFilename = "consignment.json"
+)
+
+func parseFile(filename string) (*pb.Consignment, error) {
+	var consignment *pb.Consignment
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	json.Unmarshal(data, &consignment)
+	return consignment, nil
+}
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Did not connected: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewShippingConsignmentClient(conn)
+
+	// Contact a server and print out its response
+	file := defaultFilename
+	if len(os.Args) > 1 {
+		file = os.Args[1]
+	}
+
+	consignment, err := parseFile(file)
+
+	if err != nil {
+		log.Fatalf("Could not parse file: %v", err)
+	}
+
+	r, err := client.CreateConsignment(context.Background(), consignment)
+	if err != nil {
+		log.Fatalf("Could not greet: %v", err)
+	}
+	log.Printf("Created: %t", r.Created)
+}
